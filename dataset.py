d7d0091d3bc3aab802e8f236eaa2ae072633526f
kaka <eechengy@gmail.com>
2021-01-21 12:32:00 +0800

    add train

--- dataset.py
+++ dataset.py
@@ -1,3 +1,52 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2021/1/21 10:26
 # @Author  : kaka
+
+import jieba
+import torchtext.data as tdata
+
+
+def jieba_cut(text):
+    return [w for w in jieba.lcut(text) if w.strip()]
+
+
+def get_data(path, batch_size, device):
+    LABEL = tdata.Field(sequential=False, use_vocab=False)
+    TEXT = tdata.Field(sequential=True, tokenize=jieba_cut, use_vocab=True)
+    train, val, test = tdata.TabularDataset.splits(
+        path=path,
+        train='train.json',
+        validation='val.json',
+        test='test.json',
+        format='json',
+        fields={
+            'text': ('text', TEXT),
+            'category_id': ('label', LABEL)
+        }
+    )
+
+    TEXT.build_vocab(train, min_freq=3)
+
+    train_iter = tdata.Iterator(
+        dataset=train,
+        batch_size=batch_size,
+        train=True,
+        device=device
+    )
+
+    val_iter = tdata.Iterator(
+        dataset=val,
+        batch_size=batch_size,
+        train=True,
+        device=device
+    )
+
+    test_iter = tdata.Iterator(
+        dataset=test,
+        batch_size=batch_size,
+        train=False,
+        sort=False,
+        device=device
+    )
+
+    return train_iter, val_iter, test_iter
--- train.py
+++ train.py
@@ -2,23 +2,99 @@
 # @Time    : 2021/1/15 15:22
 # @Author  : kaka
 
+import os
 import torch
-import argparse
+import torch.nn.functional as F
 import json
 from model.textcnn import TextCNN
+import dataset
 
-with open('./conf/textcnn_conf.json', 'r') as h_in:
-    args = json.load(h_in)
 
-model = TextCNN(
-    vocab_size=args['vocab_size'],
-    class_num=args['class_num'],
-    embed_size=args['embed_size'],
-    kernel_num=args['kernel_num'],
-    kernel_sizes=args['kernel_sizes'],
-    dropout_rate=args['dropout_rate']
-)
-print(model)
+def train_model(train_iter, val_iter, model, args):
+    optimizer = torch.optim.Adam(model.parameters(), lr=args['lr'])
+    step_idx = 0
+    best_acc = 0
+    best_step = 0
+    model.train()
+    for epoch in range(1, args['epochs'] + 1):
+        for batch in train_iter:
+            feature, target = batch.text, batch.label
+            # feature = feature.data.t_()
+            feature = feature.transpose(0, 1)
+            optimizer.zero_grad()
+            logits = model(feature)
+            loss = F.cross_entropy(logits, target)
+            loss.backward()
+            optimizer.step()
+            step_idx += 1
+            if step_idx % args['display_interval'] == 0:
+                corrects = (torch.max(logits, 1)[1].view(target.size()).data == target.data).sum()
+                train_acc = 100.0 * corrects / batch.batch_size
+                print('Batch[{}] - loss: {:.6f}  acc: {:.4f}%({}/{})'.format(step_idx,
+                                                                             loss.item(),
+                                                                             train_acc,
+                                                                             corrects,
+                                                                             batch.batch_size))
+            if step_idx % args['val_interval'] == 0:
+                val_acc = eval_model(val_iter, model, args)
+                if val_acc > best_acc:
+                    best_acc = val_acc
+                    best_step = step_idx
+                    print('Saving best model, acc: {:.4f}%\n'.format(best_acc))
+                    save(model, './', best_acc, step_idx)
+                else:
+                    if step_idx - best_step >= args['early_stopping']:
+                        print('early stop by {} steps, acc: {:.4f}%'.format(args['early_stopping'], best_acc))
+                        raise KeyboardInterrupt
 
-x = torch.randint(0, args['vocab_size'], (3, 10))
-print(model(x))
+
+def eval_model(data_iter, model, args):
+    model.eval()
+    corrects, avg_loss = 0, 0
+    for batch in data_iter:
+        feature, target = batch.text, batch.label
+        # feature.data.t_()
+        feature = feature.transpose(0, 1)
+        logits = model(feature)
+        loss = F.cross_entropy(logits, target)
+        avg_loss += loss.item()
+        corrects += (torch.max(logits, 1)[1].view(target.size()).data == target.data).sum()
+    size = len(data_iter.dataset)
+    avg_loss /= size
+    accuracy = 100.0 * corrects / size
+    print('\nEvaluation - loss: {:.6f}  acc: {:.4f}%({}/{}) \n'.format(avg_loss,
+                                                                       accuracy,
+                                                                       corrects,
+                                                                       size))
+    return accuracy
+
+
+def save(model, save_dir, acc, step):
+    if not os.path.isdir(save_dir):
+        os.makedirs(save_dir)
+    save_path = '{0}/step{1}_acc{2:.4f}.pt'.format(save_dir, step, acc)
+    torch.save(model.state_dict(), save_path)
+
+
+def main():
+    with open('./conf/textcnn_conf.json', 'r') as h_in:
+        args = json.load(h_in)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    train_iter, val_iter, test_iter = dataset.get_data('./data', batch_size=args['batch_size'], device=device)
+
+    vocab_size = len(train_iter.dataset.fields['text'].vocab.stoi)
+    model = TextCNN(
+        vocab_size=vocab_size,
+        class_num=args['class_num'],
+        embed_size=args['embed_size'],
+        kernel_num=args['kernel_num'],
+        kernel_sizes=args['kernel_sizes'],
+        dropout_rate=args['dropout_rate']
+    )
+    print(model)
+
+    train_model(train_iter=train_iter, val_iter=val_iter, model=model, args=args)
+
+
+if __name__ == '__main__':
+    main()
